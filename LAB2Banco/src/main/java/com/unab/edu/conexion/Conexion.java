9f9ece9e520abd7571b1e677f92424b3a179f43d
Skevin07 <63690856+Skevin07@users.noreply.github.com>
2020-10-25 11:26:56 -0600

    Creacion de clase conexion
    Ademas, creacion de las carpetas ordenadas

---
+++ LAB2Banco/src/main/java/com/unab/edu/conexion/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author kevin
+ */
+public class Conexion {
+    private Connection conexion;
+    public Conexion(){
+        try {
+            conexion=DriverManager.getConnection("jdbc:mysql://localhost/bancoo"+"?useTimezone=true&serverTimezone=UTC","root","root");
+            System.out.println("Conexion Existosa!");
+        } catch (Exception e) {
+            System.out.println("No se pudo realizar la Conexion "+e);
+        }
+    }
+    public Connection retornarConexion(){
+        return conexion;
+    }  
+}
